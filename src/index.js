78e3e75d5d745b5591b996a4030906f15e542272
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-09 16:37:25 +0300

    fix: navigation form

--- src/index.js
+++ src/index.js
@@ -5,47 +5,48 @@ const initialCards = [
         name: 'Бублики',
         link: 'https://images.unsplash.com/photo-1636178942520-3d3da95e3922?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=465&q=80',
         description: 'Очень вкусные, выского качества тесто',
-        price: '50руб.'
+        price: '50 руб.'
     },
     {
         name: 'Пиво',
         link: 'https://images.unsplash.com/photo-1636217707701-3b269167785e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
         description: 'Всегда холодное, не вызывает похмелья',
-        price: '50руб.'
+        price: '50 руб.'
     },
     {
         name: 'Чай',
         link: 'https://images.unsplash.com/photo-1635915575783-2cc97830a1a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1915&q=80',
         description: 'Чай, индийский чай, грузинский чай.',
-        price: '50руб.'
+        price: '50 руб.'
     },
     {
         name: 'Вода',
         link: 'https://images.unsplash.com/photo-1635980608878-64bb315f68b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=382&q=80',
         description: 'Полезная,',
-        price: '50руб.'
+        price: '50 руб.'
     },
     {
         name: 'Мороженое',
         link: 'https://images.unsplash.com/photo-1635749712095-45327be32d38?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
         description: 'ВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусноеВкусное',
-        price: '50руб.'
+        price: '50 руб.'
     },
     {
         name: 'Яблочный сок',
         link: 'https://images.unsplash.com/photo-1622237809626-f59a58170a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
         description: 'Свежий сок из настоящих яблок',
-        price: '50руб.'
+        price: '50 руб.'
     }
 ];
 
 const cardsTemplate = document.querySelector('.template-cards');
 const cardsContainer = document.querySelector('.cards');
-const navigation = document.querySelector('.content__navigation');
-const navCardNameInput = navigation.querySelector('.nav__form_type_name input[name="name"]');
-const navCardDescriptionInput = navigation.querySelector('.nav__form_type_description textarea[name="description"]');
-const navCardUrlInput = navigation.querySelector('.nav__form_type_url input[name="url"]');
-const navCardPriceInput = navigation.querySelector('.nav__form_type_price input[name="price"]');
+const navForm = document.querySelector('.nav__form');
+const navCardNameInput = navForm.querySelector('input[name="name"]');
+const navCardDescriptionInput = navForm.querySelector('textarea[name="description"]');
+const navCardUrlInput = navForm.querySelector('input[name="url"]');
+const navCardPriceInput = navForm.querySelector('input[name="price"]');
+const navButton = navForm.querySelector('.nav__button');
 
 function renderElement() {
     const listItems = initialCards.map(generateCard);
@@ -71,9 +72,12 @@ function addNewCard(evt) {
     const inputName = navCardNameInput.value;
     const inputImage = navCardUrlInput.value;
     const inputDescription = navCardDescriptionInput.value;
-    const inputPrice = navCardPriceInput.value;
+    const inputPrice = `${navCardPriceInput.value} руб.`;
     const newItem = generateCard({ name: inputName, link: inputImage,
          description: inputDescription, price: inputPrice });
     cardsContainer.prepend(newItem);
 }
+
+navForm.addEventListener('submit', addNewCard);
+
 
