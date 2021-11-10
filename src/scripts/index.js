daace44c68c4d8bf3af2122b514cb088463b1c72
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-10 18:13:23 +0300

    feat: some file structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
-import './main.scss';
+import '../style/main.scss';
+import './validation.js';
 
 const initialCards = [
     {
@@ -46,7 +47,6 @@ const navCardNameInput = navForm.querySelector('input[name="name"]');
 const navCardDescriptionInput = navForm.querySelector('textarea[name="description"]');
 const navCardUrlInput = navForm.querySelector('input[name="url"]');
 const navCardPriceInput = navForm.querySelector('input[name="price"]');
-const inputArr = navForm.querySelectorAll('input');
 
 const renderElement = () => {
     const listItems = initialCards.map(generateCard);
@@ -83,63 +83,3 @@ const addNewCard = (evt) => {
 
 
 navForm.addEventListener('submit', addNewCard);
-
-
-const showError = (form, formElement, errorMessage) => {
-    const errorElement = form.querySelector(`.${formElement.id}-error`);
-    formElement.classList.add('form__input_type_error');
-    errorElement.classList.add('form__input-error_active');
-    errorElement.textContent = errorMessage;
-}
-const hideError = (form, formElement) => {
-    const errorElement = form.querySelector(`.${formElement.id}-error`);
-    formElement.classList.remove('form__input_type_error');
-    errorElement.classList.remove('form__input-error_active');
-    errorElement.textContent = '';
-}
-
-const checkInputValidity = (formElement, inputElement) => {
-    if (!inputElement.validity.valid) {
-        showError(formElement, inputElement, inputElement.validationMessage)
-    } else {
-        hideError(formElement, inputElement)
-    }
-}
-
-const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => {
-        return !inputElement.validity.valid
-    })
-    }
-    
-    const toggleButtonState = (inputList, buttonElement) => {
-        if (hasInvalidInput(inputList)) {
-            buttonElement.classList.add('nav__button_inactive')
-        } else {
-            buttonElement.classList.remove('nav__button_inactive');
-        }
-    }
-    
-
-const setEventListeners = (formElement) => {
- const inputList = Array.from(formElement.querySelectorAll('input'));
- const navButton = formElement.querySelector('.nav__button');
- toggleButtonState(inputList, navButton);
- inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-        checkInputValidity(formElement, inputElement);
-        toggleButtonState(inputList, navButton);
-    })
- })
-}
-
-const enableValidation = () => {
-    const formList = Array.from(document.querySelectorAll('.nav__form'));
-    formList.forEach((formElement) => {
-        formElement.addEventListener('submit', (evt) => {
-            evt.preventDefault();
-        })
-        setEventListeners(formElement);
-    })
-}
-enableValidation()
